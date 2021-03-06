89bdfc2a113fde6657d3538f0297b00afc64dddf
UpperSideBand <63129705+UpperSideBand@users.noreply.github.com>
2021-03-06 10:36:16 +0000

    show/closeModal & EvListeners added

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// DOM links
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//Functions
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+function showModal() {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// Event Listeners
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
